2375372518e3dea64087bf95807abc30ac1ca4e0
camilo <juan.salazar@techandsolve.com>
2019-06-14 09:26:24 -0500

    primer comit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/crud'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME_HERE/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/crud'));
 });
 
 // default Heroku PORT
