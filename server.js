5998cb7e8763d6c405b272903abafbcf3787d8ed
camilo <juan.salazar@techandsolve.com>
2019-06-14 10:01:45 -0500

    primer comit

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist/crud'));
-app.get('*', function (req, res) {
-    res.sendFile('index.html');
-  });
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/{{crud}}'));
 
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/{{crud}}/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
