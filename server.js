89a3b3a33691537be5fd4c491e6e6fe0d173ff3d
camilo <juan.salazar@techandsolve.com>
2019-06-14 09:52:26 -0500

    primer comit

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/crud'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/crud/index.html'));
-
-});
+app.use(express.static('build'));
+app.get('*', function (req, res) {
+    res.sendFile('index.html');
+  });
 
 app.listen(process.env.PORT || 8080);
